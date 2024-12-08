abd211845960a9c2cef2ce26edec6aa67478925c
SUNIL KUMAR <SUNIL KUMAR@sunil>
2024-12-08 23:16:24 +0530

    This is first commit

---
+++ src/com/coding/Testcase.java
@@ -0,0 +1,23 @@
+package com.coding;
+
+public class Testcase
+{
+	
+	public static void main(String[] args)
+	{
+		
+	String str = "Sunil Kuamr";
+	
+	String[] str1 = str.split(" ");
+	
+	
+	System.out.print(str1[1]+ " "+ str1[0]);
+	
+	
+	
+	}
+	
+	
+	
+
+}
